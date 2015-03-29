2bf9d79f4acca3eee5135e6c810c5727ac2d09e3
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-03-29 19:37:16 +0300

    added callback to load_all

--- facebook-load-all.js
+++ facebook-load-all.js
@@ -1,12 +1,14 @@
 javascript: {
-  function load_all() {
+  function load_all(callback) {
     var i = setInterval(function() {
               if (document.querySelectorAll("._359.img").length == 1
                   || (document.querySelectorAll("#browse_end_of_results_footer").length == 0
                       && document.querySelectorAll("._akq").length == 1))
                 window.scrollTo(0, document.body.scrollHeight);
-              else
+              else {
                 clearInterval(i);
+                callback();
+              }
             },
             100);
   }
