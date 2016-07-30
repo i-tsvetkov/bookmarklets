157b21ac71badf885b27db426fd227e8e2952990
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-07-30 17:30:02 +0300

    update colors

--- center-image.user.js
+++ center-image.user.js
@@ -6,12 +6,12 @@
 
 if (document.images.length === 1 && document.images[0].src === document.URL)
 {
-  document.body.style.background     = 'black';
+  document.body.style.background     = '#212121';
   document.body.style.display        = 'flex';
   document.body.style.flexDirection  = 'column';
   document.body.style.justifyContent = 'center';
   document.body.style.alignItems     = 'center';
-  document.images[0].style.background = 'white';
+  document.images[0].style.background = '#FAFAFA';
   document.images[0].style.margin     = 'auto';
 }
 
