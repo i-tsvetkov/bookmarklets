ac3126467c25665d7cfd7a15016cfc8d4ed107c3
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-07-31 16:24:57 +0300

    bugfix check for empty or missing src attribute

--- center-image.user.js
+++ center-image.user.js
@@ -4,7 +4,9 @@
 // @match file://*/*
 // ==/UserScript==
 
-if (document.images.length === 1 && document.images[0].src === document.URL)
+if (document.images.length === 1 && document.images[0].src === document.URL
+    && document.images[0].hasAttribute('src')
+    && document.images[0].getAttribute('src') !== '')
 {
   document.body.style.background     = '#212121';
   document.body.style.display        = 'flex';
